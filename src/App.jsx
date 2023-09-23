7fdfb7fe4e1087d3e1922edef7b366ea56d2bd9a
JackCT1 <jackthomas933@gmail.com>
2023-09-23 17:15:47 +0100

    student register component divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 import "./App.css";
 import { hogwartsStudentRegistry } from "./data";
+import Student from "./components/student";
 
 function App() {
   return (
     <div className="App">
       <header className="title">Potions Class</header>
       <div className="app-wrapper">
-        <h1>Insert code here!</h1>
+        <div className="app-lhs-container"></div>
+        <div className="app-rhs-container">
+          <div className="register-wrapper">
+            <h1 id="register-component-header">Register</h1>
+            <div className="register-list">
+              <Student registry={hogwartsStudentRegistry} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
