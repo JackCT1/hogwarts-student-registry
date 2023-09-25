574593b5b8efdd2285a8b499aad1b4aca6c630d3
JackCT1 <jackthomas933@gmail.com>
2023-09-25 21:32:24 +0100

    house input and submit button

--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -4,7 +4,11 @@ export default function StudentForm() {
       <form className="attendance-form">
         <label htmlFor="name">Name</label>
         <br />
-        <input type="text" id="name" name="name" />
+        <input type="text" id="name" name="name" className="form-field" />
+        <label htmlFor="house">House</label>
+        <br />
+        <input type="text" id="house" name="house" className="form-field" />
+        <input type="submit" value={"Register me!"} className="submit-btn" />
       </form>
     </div>
   );
