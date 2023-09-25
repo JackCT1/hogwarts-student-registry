7e6bae738c6c8830a4571191757b8ef3572e48b2
JackCT1 <jackthomas933@gmail.com>
2023-09-25 21:26:37 +0100

    text name input

--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -1,1 +1,11 @@
-export default function StudentForm() {}
+export default function StudentForm() {
+  return (
+    <div className="form-wrapper">
+      <form className="attendance-form">
+        <label htmlFor="name">Name</label>
+        <br />
+        <input type="text" id="name" name="name" />
+      </form>
+    </div>
+  );
+}
