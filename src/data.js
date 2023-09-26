746215ce4df5889ae7c20526f39bd24bee74497f
JackCT1 <jackthomas933@gmail.com>
2023-09-26 21:02:18 +0100

    added ids to data entries

--- src/data.js
+++ src/data.js
@@ -1,35 +1,41 @@
 export const hogwartsStudentRegistry = [
   {
+    id: 1,
     name: "Harry Potter",
     house: "Gryffindor",
     time: new Date().toUTCString(),
     isLate: false,
   },
   {
+    id: 2,
     name: "Ron Weasley",
     house: "Gryffindor",
     time: new Date().toUTCString(),
     isLate: true,
   },
   {
+    id: 3,
     name: "Hermione Granger",
     house: "Gryffindor",
     time: new Date().toUTCString(),
     isLate: false,
   },
   {
+    id: 4,
     name: "Draco Malfoy",
     house: "Slytherin",
     time: new Date().toUTCString(),
     isLate: false,
   },
   {
+    id: 5,
     name: "Pansy Parkinson",
     house: "Slytherin",
     time: new Date().toUTCString(),
     isLate: false,
   },
   {
+    id: 6,
     name: "Cho Chang",
     house: "Ravenclaw",
     time: new Date().toUTCString(),
