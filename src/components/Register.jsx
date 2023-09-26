bb51857fefea524e7a97a52edb0a825b14613468
JackCT1 <jackthomas933@gmail.com>
2023-09-26 23:23:21 +0100

    register component started

---
+++ src/components/Register.jsx
@@ -0,0 +1,10 @@
+import Student from "./Student";
+
+export default function Register() {
+  return (
+    <div className="register-wrapper">
+      <h1 id="register-component-header">Register</h1>
+      <div className="register-list">{studentComponents}</div>
+    </div>
+  );
+}
