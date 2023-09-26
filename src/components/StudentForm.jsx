6573b2302330d3656587eec462df715a6b28490d
JackCT1 <jackthomas933@gmail.com>
2023-09-26 20:32:22 +0100

    adding addStudent prop

--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -1,13 +1,15 @@
-export default function StudentForm() {
+export default function StudentForm({ addStudent }) {
   return (
     <div className="form-wrapper">
-      <form className="attendance-form">
+      <form className="attendance-form" onSubmit={addStudent}>
         <label htmlFor="name">Name</label>
         <br />
         <input type="text" id="name" name="name" className="form-field" />
+        <br />
         <label htmlFor="house">House</label>
         <br />
         <input type="text" id="house" name="house" className="form-field" />
+        <br />
         <input type="submit" value={"Register me!"} className="submit-btn" />
       </form>
     </div>
