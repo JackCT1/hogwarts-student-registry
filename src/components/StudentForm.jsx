dd959af17aec904d94ee792fd004bdb11bbc2474
JackCT1 <jackthomas933@gmail.com>
2023-09-26 20:34:31 +0100

    added for-item class name to elements

--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -2,15 +2,33 @@ export default function StudentForm({ addStudent }) {
   return (
     <div className="form-wrapper">
       <form className="attendance-form" onSubmit={addStudent}>
-        <label htmlFor="name">Name</label>
+        <label htmlFor="name" className="form-item">
+          Name
+        </label>
         <br />
-        <input type="text" id="name" name="name" className="form-field" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className="form-item form-field"
+        />
         <br />
-        <label htmlFor="house">House</label>
+        <label htmlFor="house" className="form-item">
+          House
+        </label>
         <br />
-        <input type="text" id="house" name="house" className="form-field" />
+        <input
+          type="text"
+          id="house"
+          name="house"
+          className="form-item form-field"
+        />
         <br />
-        <input type="submit" value={"Register me!"} className="submit-btn" />
+        <input
+          type="submit"
+          value={"Register me!"}
+          className="form-item submit-btn"
+        />
       </form>
     </div>
   );
