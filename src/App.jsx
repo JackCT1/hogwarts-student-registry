f5a6e27a890d52df273ae12696193e366dde85fc
JackCT1 <jackthomas933@gmail.com>
2023-09-27 12:35:23 +0100

    moved register elements into separate component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { hogwartsStudentRegistry } from "./data";
 import Student from "./components/Student";
 import StudentForm from "./components/StudentForm";
+import Register from "./components/Register";
 
 function App() {
   const [students, setStudents] = useState(hogwartsStudentRegistry);
@@ -39,15 +40,10 @@ function App() {
       <header className="title">Potions Class</header>
       <div className="app-wrapper">
         <div className="app-lhs-container">
-          <div className="form-wrapper">
-            <StudentForm addStudent={addStudent} />
-          </div>
+          <StudentForm addStudent={addStudent} />
         </div>
         <div className="app-rhs-container">
-          <div className="register-wrapper">
-            <h1 id="register-component-header">Register</h1>
-            <div className="register-list">{studentComponents}</div>
-          </div>
+          <Register studentComponents={studentComponents} />
         </div>
       </div>
     </div>
