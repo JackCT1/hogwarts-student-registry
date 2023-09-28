30be93243dd30410880cf8637089759c1f13db92
JackCT1 <jackthomas933@gmail.com>
2023-09-28 13:45:42 +0100

    all house badge images working

--- src/components/student.jsx
+++ src/components/student.jsx
@@ -1,9 +1,19 @@
 export default function Student({ name, house, time }) {
   return (
     <div className="student-record">
-      <h2 className="student-record-student-name">{name}</h2>
-      <p>{house}</p>
-      <p>{time}</p>
+      <div className="student-record-info">
+        <div className={"student-record-house-badge"}>
+          <img
+            src={`../../public/images/${house}.png`}
+            alt="House Badge"
+            width={100}
+            height={100}
+          />
+        </div>
+        <h2 className="student-record-student-name">{name}</h2>
+        <p>{house}</p>
+        <p>{time}</p>
+      </div>
     </div>
   );
 }
